aac674c982a08f4710ab07e79cfebd78dc273e8e
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-10-22 16:21:15 -0700

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -47,25 +47,22 @@ int mydgetrf(double *A, int *ipiv, int n)
 //                 max = abs(A[t*n+i]); 
             }
         }
+        if (max==0) 
+            return -1; 
+        else if (maxind != i ) {
+            temps = ipiv[i]; 
+            ipiv[i] = ipiv[maxind]; 
+            ipiv[maxind] = temps;
+            for (a=0 ; a <=n ; a++) tempv[a] = A[i*n+a]; 
+            for (b=0 ; b <=n ; b++) A[i*n+b] = A[maxind*n+b]; 
+            for (c=0 ; c <=n ; c++) A[maxind*n+c] = tempv[c];
+        }
+        for (j = i+1 ; j <= n ; j++) {
+            A[j*n+i] = A[j*n+i]/A[i*n+i];
+            for (k = i+1 ; k <=n ; k++)
+                A[j*n+k] = A[j*n+k] - A[j*n+i] * A[i*n+k]; 
+        } 
     }
-    
-//     if (max==0) 
-//         return -1; 
-//     else if (maxind != i ) {
-//         temps = ipiv[i]; 
-//         ipiv[i] = ipiv[maxind]; 
-//         ipiv[maxind] = temps;
-//         for (a=0 ; a <=n ; a++) tempv[a] = A[i*n+a]; 
-//         for (b=0 ; b <=n ; b++) A[i*n+b] = A[maxind*n+b]; 
-//         for (c=0 ; c <=n ; c++) A[maxind*n+c] = tempv[c];
-//     }
-    
-//     for (j = i+1 ; j <= n ; j++) {
-//         A[j*n+i] = A[j*n+i]/A[i*n+i];
-//         for (k = i+1 ; k <=n ; k++)
-//             A[j*n+k] = A[j*n+k] - A[j*n+i] * A[i*n+k]; 
-//     } 
-    
     return 0;
 }
 
