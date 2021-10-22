89d2c7ea7225c8d13a3067e37f219c33dde98322
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-10-22 01:45:09 -0700

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -41,10 +41,10 @@ int mydgetrf(double *A, int *ipiv, int n)
         maxind = i; 
         max = abs(A[i*n+i]); 
         for (t = i+1 ; t <= n ; t++){
-//             if (abs(A[t*n+i]) > max ) {
+            if (abs(A[t*n+i]) > max ) {
 //                 maxind = t; 
 //                 max = abs(A[t*n+i]); 
-//             }
+            }
         }
     }
     
