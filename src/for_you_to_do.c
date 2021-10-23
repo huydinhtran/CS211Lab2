49079aa95ae5cf639cb6a51346e605821783c3e3
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-10-22 18:04:16 -0700

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -98,7 +98,7 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
     /* add your code here */
     //forward substitution for lower triangular
     double y[n]; 
-    y[n] = (double*) malloc (n * sizeof(double));
+    y[n] = malloc (n * sizeof(double));
     double x[n];
 
     if (UPLO == 'L'){
