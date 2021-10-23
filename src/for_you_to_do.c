a6c61eb48e31eb26953a400ace758735541c1fd6
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-10-22 17:12:44 -0700

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -31,38 +31,38 @@ int get_block_size(){
 
 int mydgetrf(double *A, int *ipiv, int n) 
 {
-    /* add your code here */
-    int maxind;
-    int temps;
-    double max;
-    double tempv[n];
-    int i, t, j, k;
-    int a, b, c;
-    for (i = 1 ; i <= n-1 ; i++){
-        maxind = i; 
-        max = abs(A[i*n+i]); 
-        for (t = i+1 ; t <= n ; t++){
-            if (abs(A[t*n+i]) > max ) {
-                maxind = t; 
-                max = abs(A[t*n+i]); 
-            }
-        }
-        if (max==0) 
-            return -1; 
-        else if (maxind != i ) {
-            temps = ipiv[i]; 
-            ipiv[i] = ipiv[maxind]; 
-            ipiv[maxind] = temps;
-            for (a=0 ; a <=n ; a++) tempv[a] = A[i*n+a]; 
-            for (b=0 ; b <=n ; b++) A[i*n+b] = A[maxind*n+b]; 
-            for (c=0 ; c <=n ; c++) A[maxind*n+c] = tempv[c];
-        }
-        for (j = i+1 ; j <= n ; j++) {
-            A[j*n+i] = A[j*n+i]/A[i*n+i];
-            for (k = i+1 ; k <=n ; k++)
-                A[j*n+k] = A[j*n+k] - A[j*n+i] * A[i*n+k]; 
-        } 
-    }
+//     /* add your code here */
+//     int maxind;
+//     int temps;
+//     double max;
+//     double tempv[n];
+//     int i, t, j, k;
+//     int a, b, c;
+//     for (i = 1 ; i <= n-1 ; i++){
+//         maxind = i; 
+//         max = abs(A[i*n+i]); 
+//         for (t = i+1 ; t <= n ; t++){
+//             if (abs(A[t*n+i]) > max ) {
+//                 maxind = t; 
+//                 max = abs(A[t*n+i]); 
+//             }
+//         }
+//         if (max==0) 
+//             return -1; 
+//         else if (maxind != i ) {
+//             temps = ipiv[i]; 
+//             ipiv[i] = ipiv[maxind]; 
+//             ipiv[maxind] = temps;
+//             for (a=0 ; a <=n ; a++) tempv[a] = A[i*n+a]; 
+//             for (b=0 ; b <=n ; b++) A[i*n+b] = A[maxind*n+b]; 
+//             for (c=0 ; c <=n ; c++) A[maxind*n+c] = tempv[c];
+//         }
+//         for (j = i+1 ; j <= n ; j++) {
+//             A[j*n+i] = A[j*n+i]/A[i*n+i];
+//             for (k = i+1 ; k <=n ; k++)
+//                 A[j*n+k] = A[j*n+k] - A[j*n+i] * A[i*n+k]; 
+//         } 
+//     }
     return 0;
 }
 
@@ -97,33 +97,33 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
 {
     /* add your code here */
     //forward substitution for lower triangular
-//     double y[n], x[n];
+    double y[n], x[n];
 
-//     if (UPLO == 'L'){
-//         int i;
-//         int a;
-//         double sum;
-//         y[1] = B[ipiv[1]];
-//         for (i=2 ; i<n ; i++){
-//             for (a=1 ; a<=i-1 ; a++){
-//                 sum += y[a] * A[i*n+a];
-//             }
-//             y[i] = B[ipiv[i]] - sum;
-//         }
-//     }
-//     //backward substitution for upper triangular
-//     if (UPLO == 'U'){
-//         int i;
-//         int a;
-//         double sum;
-//         x[n] = y[n] / A[n*n+n];
-//         for (i=n-1 ; i>1 ; i--){
-//             for (a=i+1 ; a <= n ; a++){
-//                 sum += x[a] * A[i*n+a];
-//             }
-//             x[i] = (y[i] - sum) / A[i*n+i];
-//         }
-//     }
+    if (UPLO == 'L'){
+        int i;
+        int a;
+        double sum;
+        y[1] = B[ipiv[1]];
+        for (i=2 ; i<n ; i++){
+            for (a=1 ; a<=i-1 ; a++){
+                sum += y[a] * A[i*n+a];
+            }
+            y[i] = B[ipiv[i]] - sum;
+        }
+    }
+    //backward substitution for upper triangular
+    if (UPLO == 'U'){
+        int i;
+        int a;
+        double sum;
+        x[n] = y[n] / A[n*n+n];
+        for (i=n-1 ; i>1 ; i--){
+            for (a=i+1 ; a <= n ; a++){
+                sum += x[a] * A[i*n+a];
+            }
+            x[i] = (y[i] - sum) / A[i*n+i];
+        }
+    }
     return;
 }
 
