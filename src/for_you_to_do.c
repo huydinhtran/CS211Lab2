209398d80b0399f4471b88f67c49047b49f2d559
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-10-22 19:38:42 -0700

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -97,37 +97,37 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
     /* add your code here */
     //forward substitution for lower triangular
 
-//     if (UPLO == 'L'){
-//         int i;
-//         int a;
-//         double sum;
-//         double* y; 
-//         y= (double*) malloc (n * sizeof(double));
-//         y[1] = B[ipiv[1]];
-//         for (i=2 ; i<=n ; i++){
-//             for (a=1 ; a<=i-1 ; a++){
-//                 sum += y[a] * A[i*n+a];
-//             }
-//             y[i] = B[ipiv[i]] - sum;
-//         }
-//     }
-//     //backward substitution for upper triangular
-//     if (UPLO == 'U'){
-//         int i;
-//         int a, b;
-//         double sum;
-//         double x[n];
-//         double* y; 
-//         y = (double*) malloc (n * sizeof(double));
-//         y[n-1]=x[n-1]/A[(n-1)*n+n-1];
-//         for (b=0 ; b<n ; b++) y[b] = x[b];
-//         for (i=n-1 ; i>=1 ; i--){
-//             for (a=i+1 ; a <= n ; a++){
-//                 sum += x[a] * A[i*n+a];
-//             }
-//             x[i] = (y[i] - sum) / A[i*n+i];
-//         }
-//     }
+    if (UPLO == 'L'){
+        int i;
+        int a;
+        double sum;
+        double* y; 
+        y= (double*) malloc (n * sizeof(double));
+        y[1] = B[ipiv[1]];
+        for (i=2 ; i<=n ; i++){
+            for (a=1 ; a<=i-1 ; a++){
+                sum += y[a] * A[i*n+a];
+            }
+            y[i] = B[ipiv[i]] - sum;
+        }
+    }
+    //backward substitution for upper triangular
+    if (UPLO == 'U'){
+        int i;
+        int a, b;
+        double sum;
+        double x[n];
+        double* y; 
+        y = (double*) malloc (n * sizeof(double));
+        y[n-1]=x[n-1]/A[(n-1)*n+n-1];
+        for (b=0 ; b<n ; b++) y[b] = x[b];
+        for (i=n-1 ; i>=1 ; i--){
+            for (a=i+1 ; a <= n ; a++){
+                sum += x[a] * A[i*n+a];
+            }
+            x[i] = (y[i] - sum) / A[i*n+i];
+        }
+    }
     return;
 }
 
