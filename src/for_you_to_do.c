d7aaa8301e4853fa894126a439c0cb1558db041b
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-10-22 19:28:24 -0700

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -120,8 +120,8 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
         double x[n];
         double* y; 
         y = (double*) malloc (n * sizeof(double));
-        x[n] = y[n] / A[n*n+n];
-        for (a=0 ; a >= n ; a++) y[i] = x[i];
+        y[n-1]=x[n-1]/A[(n-1)*n+n-1]
+        for (a=0 ; a<n ; a++) y[i] = x[i];
         for (i=n-1 ; i>=1 ; i--){
             for (a=i+1 ; a <= n ; a++){
                 sum += x[a] * A[i*n+a];
