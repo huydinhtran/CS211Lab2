c68f9d5b62a59de72bde1840703788afca20fcff
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-10-29 01:32:40 -0700

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -305,7 +305,7 @@ int mydgetrf_block(double *A, int *ipiv, int n, int b)
             for (j = end+1 ; j < n ; j++)
                 A3[i*n+j] = A[i*n+j];
         
-        mydgemm(*A3, *A2, *A1, n, 0, 0, 0, b);
+        mydgemm(A3, A2, A1, n, 0, 0, 0, b);
         
         for (i = 0 ; i < n ; i++)
             for (j = 0 ; j < n ; j++)
