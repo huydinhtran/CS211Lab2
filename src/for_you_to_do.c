7b5939be459f680d3db5cba46f288962c46e4957
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-10-29 17:48:49 -0700

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -237,9 +237,9 @@ int mydgetrf_block(double *A, int *ipiv, int n, int b)
                 temps = ipiv[i]; 
                 ipiv[i] = ipiv[maxind]; 
                 ipiv[maxind] = temps;
-                for (x=0 ; x < n ; x++) tempv[x] = A[i*n+x]; 
-                for (y=0 ; y < n ; y++) A[i*n+y] = A[maxind*n+y]; 
-                for (z=0 ; z < n ; z++) A[maxind*n+z] = tempv[z];
+                for (x=0 ; x < end ; x++) tempv[x] = A[i*n+x]; 
+                for (y=0 ; y < end ; y++) A[i*n+y] = A[maxind*n+y]; 
+                for (z=0 ; z < end ; z++) A[maxind*n+z] = tempv[z];
             }
             for (j = i+1 ; j < n ; j++) { 
                 A[j*n+i] = A[j*n+i]/A[i*n+i];
